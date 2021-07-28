00f285afda5f72116f6967f3662586ef9bcd8843
Maylani Kusuma Wardhani <maylaniwardhani@gmail.com>
2021-07-28 15:58:43 +0700

    Update Login Page

--- pages/login.vue
+++ pages/login.vue
@@ -1,28 +1,27 @@
 <template>
+<div>
     <div class="container-fluid">
-        <div class="row justify-content-center" style="margin: 0 25%">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="row justify-content-center" style="">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-9" style="position: absolute; top: 25%; left: 25%;">
                 <div class="card o-hidden border-3 shadow-lg my-3">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <div class="card-body p-0" style="">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                            </div>
+                                <form class="user">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address">
                                     </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Enter Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Enter Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember Me</label>
-                                            </div>
                                         </div>
+                                    </div>
                                         <a href="javascript:void(0)" @click="submit" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </a>
@@ -33,7 +32,7 @@
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a>
-                                    </form>
+                                </form>
                                     <hr>
                                     <div class="text-center">
                                         <a class="small" href="#">Forgot Password?</a>
@@ -41,14 +40,13 @@
                                     <div class="text-center">
                                         <a class="small" href="#">Create an Account!</a>
                                     </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 <script>
 export default{
