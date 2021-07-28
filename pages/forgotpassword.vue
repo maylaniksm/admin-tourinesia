c84bb3ea8c28f5793a48d5412b13e28e64ecf0cc
Maylani Kusuma Wardhani <maylaniwardhani@gmail.com>
2021-07-28 16:23:56 +0700

    Update Forgot Setup Password Login & Register Page

--- pages/forgotpassword.vue
+++ pages/forgotpassword.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="container-fluid">
             <div class="row justify-content-center">
-                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-9" style="position: absolute; top: 25%; left: 25%;">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-9" style="position: absolute; top: 25%;">
                     <div class="card o-hidden border-3 shadow-lg my-3">
                         <div class="card-body p-0">
                             <div class="row">
--- pages/register.vue
+++ pages/register.vue
@@ -1,21 +1,20 @@
 <template>
+<div>
     <div class="container-fluid">
-        <div class="row justify-content-center" style="margin: 0 25%">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="row justify-content-center" style="">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-9" style="position: absolute; top: 25%;">
                 <div class="card o-hidden border-3 shadow-lg my-3">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">REGISTER</h1>
-                                    </div>
-                                    <form class="user">
-                                         <div class="form-group">
+                    <div class="card-body p-0" style="">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">REGISTER</h1>
+                            </div>
+                                <form class="user">
+                                        <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="exampleInputName" placeholder="Name">
                                         </div>
                                         <div class="form-group">
-                                            <input type="number" class="form-control form-control-user" id="exampleInputPhoneNumber" placeholder="Phone Number">
+                                            <input type="text" class="form-control form-control-user" id="exampleInputPhoneNumber" placeholder="Phone Number">
                                         </div>
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email Address">
@@ -36,20 +35,23 @@
                                             Register
                                         </a>
                                         <hr>
-                                    </form>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                          <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                </form>
                                     <div class="text-center">
                                         <a class="small" href="#">Back to Login!</a>
                                     </div>
-                                    <hr>
-                                    
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 <script>
 export default{
